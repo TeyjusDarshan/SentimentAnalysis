fixed attention mask logic

layers/attention_head.py
@@ -3,14 +3,13 @@ import torch
 import math
 
 class AttentionHead(nn.Module):
-    def __init__(self, model_embed_size, output_embed_size, attention_mask = None):
+    def __init__(self, model_embed_size, output_embed_size):
         super().__init__()
 
         self.qW = nn.Linear(model_embed_size, output_embed_size) #shape (C, Oc)
         self.kW = nn.Linear(model_embed_size, output_embed_size)
         self.vW = nn.Linear(model_embed_size, output_embed_size)
         self.output_embed_size = output_embed_size
-        self.mask = attention_mask
 
         #scaling of weights
         nn.init.normal_(self.qW.weight, mean = 0.0, std = 0.02)
@@ -24,18 +23,20 @@ class AttentionHead(nn.Module):
 
 
     
-    def forward(self, x):
+    def forward(self, x, attention_mask = None):
         #shape of x is (B, T, C)
         Q = self.qW(x) #shape (B, T, Oc)
         K = self.kW(x)
         V = self.vW(x) #shape (B, T, Oc)
 
         attention = Q @ K.transpose(-1, -2) / math.sqrt(self.output_embed_size)#shape (B, T, T)
-        if self.mask is not None:
-            mask = self.mask.unsqueeze(1)
+        if attention_mask is not None:
+            mask = attention_mask.unsqueeze(1)
             attention = attention.masked_fill(mask == 0, float('-inf'))
 
-        output = attention @ V #shape (B, T, Oc)
+        attention_probs = torch.softmax(attention, dim = 1)
+
+        output = attention_probs @ V #shape (B, T, Oc)
         
         return output
 

layers/encoder.py
@@ -21,8 +21,8 @@ class Encoder(nn.Module):
                 nn.init.normal_(layer.weight, 0, 0.02)
                 nn.init.constant_(layer.bias, 0)
     
-    def forward(self, x):
-        x = self.mlh(x)
+    def forward(self, x, attention_mask = None):
+        x = self.mlh(x, attention_mask)
 
         ln_res = self.ln2(x)
         y = self.ff(ln_res)

layers/multihead_attention.py
@@ -15,9 +15,9 @@ class MultiheadAttention(nn.Module):
         nn.init.normal_(self.projection_layer.weight, 0, 0.02)
         nn.init.constant_(self.projection_layer.bias, 0)
     
-    def forward(self, x):
+    def forward(self, x, attention_mask = None):
         norm_x = self.ln(x)
-        head_outputs = [h(norm_x) for h in self.attention_heads]
+        head_outputs = [h(norm_x, attention_mask) for h in self.attention_heads]
         out = torch.cat(head_outputs, dim = -1) #shape (B, T, model_embed_size)
         projection = self.projection_layer(out)
         residual_output =  x + projection

model/SentiNa.py
@@ -31,13 +31,26 @@ class SentiNa(nn.Module):
 
         x = x + pos_enc
 
+
+
         for encoder in self.encoders:
-            x = encoder(x)
+            x = encoder(x, attention_mask)
+        
+
         
         #final shape of the output is (B, T, model_dim)
         #average across tokens
 
-        x = torch.mean(x, dim = -2) # output (B, model_dim)
+        mask = attention_mask.unsqueeze(-1).float()
+
+        x = x * mask
+
+        sum_x = torch.sum(x, dim = 1)
+
+        token_counts = torch.clamp(mask.sum(dim=-2), min=1e-9)
+
+        x = sum_x/token_counts
+
 
         x = self.classifier(x)
 